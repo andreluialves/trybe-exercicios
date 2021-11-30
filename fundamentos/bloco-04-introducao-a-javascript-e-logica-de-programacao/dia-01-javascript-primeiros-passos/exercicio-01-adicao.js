8983041d82eb1c43e513f3f80bd71e70cba70dc4
André Alves <andreluialves@hotmail.com>
2021-11-30 15:52:47 -0300

    Nome da pasta do bloco 4 renomeada

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-01-adicao.js
@@ -0,0 +1,9 @@
+/* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , 
+definidas no começo com os valores que serão operados. 
+*/
+
+const a = 30;
+const b = 10;
+
+// Adição
+console.log("Resultado da adição: " + (a + b));
