d5583b6200dd2dddee878ddf564bb665a7dd03eb
André Alves <andreluialves@hotmail.com>
2022-02-11 11:36:20 -0300

    Exercício 9 feito

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-09.js
@@ -0,0 +1,8 @@
+let result = [];
+for (let index = 1; index <= 25; index += 1) {
+  result.push(index);
+}
+
+for (let index = 0; index < result.length; index++) {
+  console.log(result[index] / 2);
+}
