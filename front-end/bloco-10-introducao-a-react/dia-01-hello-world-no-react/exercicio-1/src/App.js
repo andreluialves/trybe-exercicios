938b885878fc1306b3e533b2d1aff05975e1df17
André Alves <andreluialves@hotmail.com>
2022-02-16 21:43:33 -0300

    Exercício 1 feito, criação de lista de tarefas

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercicio-1/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tasks = ['Estudar', 'Varrer a casa', 'Molhar as plantas', 'Ir ao mercado'];
+
+function App() {
+  return (
+    <ul>{tasks.map(Task)}</ul>
+  );
+}
+
+export default App;
