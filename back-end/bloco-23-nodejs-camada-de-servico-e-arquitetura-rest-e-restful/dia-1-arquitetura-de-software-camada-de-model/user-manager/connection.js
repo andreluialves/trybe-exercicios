b9e8e495ebef77846e966f16cd289a8e25db8729
André Alves <andreluialves@hotmail.com>
2022-06-28 01:37:07 -0300

    refactor: alteração da porta de comunicação com o banco de dados

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/user-manager/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-const connection = mysql.createPool({
-  user: 'root',
-  password: 'root',
-  host: 'localhost',
-  database: 'users_crud',
-  port: 3000
-});
